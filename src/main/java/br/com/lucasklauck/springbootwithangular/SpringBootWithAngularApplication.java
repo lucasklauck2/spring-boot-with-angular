0b2c6d3c09626ce3b408a2d6f471dc8f4495c41e
Lucas Eduardo <lucasklauck120@gmail.com>
2022-10-09 21:42:18 -0300

    Initial commit

---
+++ src/main/java/br/com/lucasklauck/springbootwithangular/SpringBootWithAngularApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucasklauck.springbootwithangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucasklauck/springbootwithangular/SpringBootWithAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasklauck.springbootwithangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

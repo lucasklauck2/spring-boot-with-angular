6e2d937d6d9406bb509e0728ff07c0357a9ef18d
Lucas Eduardo <lucasklauck120@gmail.com>
2022-10-09 22:07:34 -0300

    Fixed white label problem

--- src/main/java/br/com/lucasklauck/springbootwithangular/SpringBootWithAngularApplication.java
+++ src/main/java/br/com/lucasklauck/springbootwithangular/SpringBootWithAngularApplication.java
@@ -3,11 +3,19 @@ package br.com.lucasklauck.springbootwithangular;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Controller
 @SpringBootApplication
 public class SpringBootWithAngularApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWithAngularApplication.class, args);
+	}
+
+ 	private static final String PATH = "/error";
+
+	@RequestMapping(value = PATH)
+	public String error() {
+        return "forward:/index.html";
 	}
 
 }
